8e51d61a22964ec42d55076b88501d57edb36ace
sumdayo <wkvmgc961uy56@gmail.com>
2026-08-19 19:41:07 +0900

    Create - pfact.hpp

---
+++ datastructure/math/pfact.hpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// 未検証
+// ここからペースト
+
+vector<int> pfact(int num){
+    vector<int> res;
+    for (int i = 2;i * i <= num;i++) {
+        while (num % i == 0) {
+            num /= i;
+            res.push_back(i);
+        }
+    }
+    if (num != 1) res.push_back(num);
+
+    return res;
+}
